e41b7559b34682a942a17dda6263bc4a9440737f
07abishake <abishake153@gmail.com>
2025-10-07 15:21:39 +0530

    UserDomian name chnage

--- src/pages/UserManagement.js
+++ src/pages/UserManagement.js
@@ -412,7 +412,7 @@ const fetchLocations = async () => {
     if (selectedDepartment) formData.append('Department', selectedDepartment);
     if (selectedDesignation) formData.append('Designation', selectedDesignation);
     formData.append('EmployeeID', employeeId);
-    formData.append('EmailId', `${email}@${data?.userDomain}`);
+    formData.append('EmailId', `${email}@${data?.domain}`);
     if (workNumber) formData.append('WorkPhone', `+${workNumber}`);
     if (phoneNumber) formData.append('PersonalMobile', `+${phoneNumber}`);
     formData.append('Extension', extension);
@@ -868,7 +868,7 @@ const fetchLocations = async () => {
             autoComplete="new-email"
             name="new-email"
                   />
-                  <span className="input-group-text">@{data?.userDomain}</span>
+                  <span className="input-group-text">@{data?.domain}</span>
                 </div>
 
                 <div className="mb-3 d-flex">
