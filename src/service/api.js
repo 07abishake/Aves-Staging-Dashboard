6d09dc397f33ad728eac17032940df77af952550
07abishake <abishake153@gmail.com>
2025-10-25 09:58:32 +0530

    api

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3393/api';
+const API_BASE_URL = 'https://codeaves.avessecurity.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
