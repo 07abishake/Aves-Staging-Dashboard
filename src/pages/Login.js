22faa4a06dcd9361eb2e57129a7b99df870e08bd
07abishake <abishake153@gmail.com>
2025-10-27 09:59:01 +0530

    Login Chnage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
 
     try {
         const response = await axios.post(
-            'http://localhost:3393/api/auth/login',
+            'https://codeaves.avessecurity.com/api/auth/login',
             { name, password },
             { headers: { 'Content-Type': 'application/json' } }
         );
